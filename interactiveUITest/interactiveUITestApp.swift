fb927b995b6f4a09d83ed386e7e7b90c8a7523da
Matt Wecht <mattwecht@gmail.com>
2023-04-04 17:56:16 -0400

    Initial Commit

---
+++ interactiveUITest/interactiveUITestApp.swift
@@ -0,0 +1,17 @@
+//
+//  interactiveUITestApp.swift
+//  interactiveUITest
+//
+//  Created by scholar on 4/4/23.
+//
+
+import SwiftUI
+
+@main
+struct interactiveUITestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
